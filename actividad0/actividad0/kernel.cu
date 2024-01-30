6e3f3291ca0ff4c35abcf5ab106d85e9ddfa3b6d
eduardogora <42094872+eduardogora@users.noreply.github.com>
2024-01-29 22:11:22 -0600

    actividad0

---
+++ actividad0/actividad0/kernel.cu
@@ -0,0 +1,14 @@
+﻿#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+__global__ void print_hello_cuda() {
+	int i = threadIdx.x;
+	printf("[PRINT] ThreadId.x: %d \n", i);
+}
+
+int main() {
+	print_hello_cuda << <1, 8 >> > ();
+	return 0;
+}
