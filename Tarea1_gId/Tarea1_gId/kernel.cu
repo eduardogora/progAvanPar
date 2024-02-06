82a69b673996707123a1be1ff6bb43b75c29ce91
eduardogora <42094872+eduardogora@users.noreply.github.com>
2024-02-05 20:22:55 -0600

    tarea01

---
+++ Tarea1_gId/Tarea1_gId/kernel.cu
@@ -0,0 +1,39 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+
+__global__ void globalId()
+{
+    int hiloX = threadIdx.x;
+    int hiloY = threadIdx.y;
+
+    int blockX = blockIdx.x;
+    int blockY = blockIdx.y;
+
+    int dimX = blockDim.x;
+    int dimY = blockDim.y;
+
+    int globalIDx = blockX * dimX + hiloX;
+    int globalIDy = blockY * dimY + hiloY;
+
+    int gId = globalIDy * blockDim.x * gridDim.x + globalIDx;
+
+    //printf("gID: %d \n", gId);
+    printf("%d \n", gId);
+}
+
+int main()
+{
+    dim3 blockSize(4, 2);
+
+    dim3 gridSize(2, 2);
+
+    globalId << <gridSize, blockSize >> > ();
+    cudaDeviceSynchronize();
+    return 0;
+}
+
